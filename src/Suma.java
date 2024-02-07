87404132cec7c0bd34f2409c7e7d46efd4f49c90
HiImGhost666 <n3twork69@gmail.com>
2024-02-07 18:32:02 +0000

    Version LUKE
    Cambie este programa a un programa que pide al usuario que introduzca una frase y que luego introduzca una palabra pa ver si está o algo ns

--- src/Suma.java
+++ src/Suma.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,11 +11,32 @@
  * @author Daw1
  */
 public class Suma {
-    int a;
-    int b;
-    
-    public int suma (int a, int b) {
-       int suma = a + b;
-       return suma;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca una frase: ");
+        String frase = sc.nextLine();
+        System.out.print("Introduzca una palabra para buscarla dentro de la frase: ");
+        String palabra = sc.next();
+
+        int veces = buscarPalabra(frase, palabra);
+
+        if (veces == 0) {
+            System.out.println(palabra +" no se encuentra en la frase");
+        } else {
+            System.out.println(palabra + " aparece " + veces + " veces en la frase");
+        }
     }
+
+    static int buscarPalabra(String frase, String palabra) {
+        int veces = 0;
+        int pos = frase.indexOf(palabra);
+
+        while (pos != -1) {
+            veces++;
+            pos = frase.indexOf(palabra, pos + 1);
+        }
+
+        return veces;
+    }
+    
 }
