8e3e08ef3b76178a75edc428ccf9eaaa4018991c
Chris1142 <christianvilvaz@hotmail.com>
2024-02-07 15:02:59 +0000

    Versión 1.0

---
+++ src/NewMain.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Daw1
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Escribe un número");
+    }
+    
+}
---
+++ src/Suma.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daw1
+ */
+public class Suma {
+    int a;
+    int b;
+    
+    public int suma (int a, int b) {
+       int suma = a + b;
+       return suma;
+    }
+}
